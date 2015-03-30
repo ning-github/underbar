6aec7fb4d371d82f6eff1731c295c826b67a0cb6
Ning Xia <nxiamail@gmail.com>
2015-03-30 15:05:19 -0700

    Completed reduce(collection, iterator, accumulator) - used _each() to iterate through and set limit on how many times iterator function runs 	- the beauty of using _.each is that it already checks for object vs array - passing an anonymous function to _.each() 	- this anonymous function is what runs accumulator = iterator(accumulator, each) 		-continues to fold until done - to account for whether an accumulator was provided (meaning start rolling from 1st or second element) 	I set the condition that if (accummulator was undefined) it would mean it wasn't passed as an 	argument and should therefore be set to "each" on that iteration, meaning true accumulation would 	not start till the second iteration.

--- src/underbar.js
+++ src/underbar.js
@@ -169,6 +169,15 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(each){
+      if (accumulator === undefined){
+        accumulator = each;
+      }
+      else {
+        accumulator = iterator(accumulator, each);
+      }
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
