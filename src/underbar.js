5b63cee0ab7c23a4872fc0a9a5b6fe3928246f4d
Ning Xia <nxiamail@gmail.com>
2015-04-06 23:24:30 -0700

    Completed _.difference(oneArray,*arraysToCompare) - used slice to get arguments as an args array - used _.last to get the arrays to compare - used _.flatten to combine the arrays to compare into one array 	- use _.filter and _.contains to create an array that filters contents that did not appear in the combined array into the output array

--- src/underbar.js
+++ src/underbar.js
@@ -510,18 +510,27 @@
             inAll=false;
         }   
       });
-      
+      //if a given element shortest[i] checked in eachArray of passedArrays without returning false
       if(inAll){
         output.push(shortest[i]);
       }
     }
-    
     return output;     
   };
 
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
+    var args = Array.prototype.slice.call(arguments);
+    
+    var oneArray = args[0];
+    var others = _.last(args, args.length-1);
+    var combined = _.flatten(others, true);
+
+    var output = _.filter(oneArray, function(each){
+      return (!_.contains(combined, each));
+    });
+    return output;
   };
 
   // Returns a function, that, when invoked, will only be triggered at most once
